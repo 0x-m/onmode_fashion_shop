eeb46e26d30179adda996316cc0180f94ed8a180
0x-m <m.ghaedi.74@gmail.com>
2021-07-10 09:40:45 +0430

    produc

--- onmode/settings.py
+++ onmode/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-klg*(0#3t#0%ddpkjfy#6ebv0%i@p+v6rsml8v+_lt!9*stupf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['onmode.herokuapp.com','localhost']
 
