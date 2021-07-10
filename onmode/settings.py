97133efeeb79aa523fb1d395bc3301bd60a290d3
0x-m <m.ghaedi.74@gmail.com>
2021-07-10 11:20:18 +0430

    dsfsf

--- onmode/settings.py
+++ onmode/settings.py
@@ -138,7 +138,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 import os
 MEDIA_URL = '/media/'
