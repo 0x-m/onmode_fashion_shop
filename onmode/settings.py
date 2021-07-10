862993c66156aeabb2b50c8b794688230397973c
0x-m <m.ghaedi.74@gmail.com>
2021-07-10 18:04:43 +0430

    sdfsfsf

--- onmode/settings.py
+++ onmode/settings.py
@@ -137,8 +137,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
-STATIC_ROOT  =  os.path.join(BASE_DIR,'static')
+# STATIC_ROOT  =  os.path.join(BASE_DIR,'staticfiles')
+# STATIC_URL = '/static/'
+PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
+STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra lookup directories for collectstatic to find static files
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
 
 STATICFILES_DIRS = [
    os.path.join(BASE_DIR, 'static')
